99912efe3303a829346bfdb4fb4eba8a9079cba8
alex-skiffin <alex@skiffin.ru>
2017-03-07 21:27:54 +0500

    change sorting

--- app/viewer/table/table.component.js
+++ app/viewer/table/table.component.js
@@ -8,55 +8,5 @@ TableComponent.annotations = [
         ]
     })
 ];
-var desc = true;
-app.TableComponent.prototype.sortByName = function () {
-    if (desc)
-        this.users.sort((a, b) => a.name == b.name ? 0 : +(a.name > b.name) || -1);
-    else
-        this.users.sort((a, b) => a.name == b.name ? 0 : +(a.name < b.name) || -1);
-    desc = !desc;
-}
-app.TableComponent.prototype.sortByAge = function () {
-    if (desc)
-        this.users.sort((a, b) => a.age == b.age ? 0 : +(a.age > b.age) || -1);
-    else
-        this.users.sort((a, b) => a.age == b.age ? 0 : +(a.age < b.age) || -1);
-    desc = !desc;
-}
-app.TableComponent.prototype.sortByGender = function () {
-    if (desc)
-        this.users.sort((a, b) => a.gender == b.gender ? 0 : +(a.gender > b.gender) || -1);
-    else
-        this.users.sort((a, b) => a.gender == b.gender ? 0 : +(a.gender < b.gender) || -1);
-    desc = !desc;
-}
-app.TableComponent.prototype.sortByDepartment = function () {
-    if (desc)
-        this.users.sort((a, b) => a.department == b.department ? 0 : +(a.department > b.department) || -1);
-    else
-        this.users.sort((a, b) => a.department == b.department ? 0 : +(a.department < b.department) || -1);
-    desc = !desc;
-}
-app.TableComponent.prototype.sortByAddress = function () {
-    if (desc)
-        this.users.sort((a, b) => a.address == b.address ? 0 : +(a.address > b.address) || -1);
-    else
-        this.users.sort((a, b) => a.address == b.address ? 0 : +(a.address < b.address) || -1);
-    desc = !desc;
-}
-app.TableComponent.prototype.sortByPhone = function () {
-    if (desc)
-        this.users.sort((a, b) => a.phone == b.phone ? 0 : +(a.phone > b.phone) || -1);
-    else
-        this.users.sort((a, b) => a.phone == b.phone ? 0 : +(a.phone < b.phone) || -1);
-    desc = !desc;
-}
-app.TableComponent.prototype.sortByEmail = function () {
-    if (desc)
-        this.users.sort((a, b) => a.email == b.email ? 0 : +(a.email > b.email) || -1);
-    else
-        this.users.sort((a, b) => a.email == b.email ? 0 : +(a.email < b.email) || -1);
-    desc = !desc;
-}
 function TableComponent() {
 }
--- app/viewer/viewer.component.js
+++ app/viewer/viewer.component.js
@@ -1,11 +1,13 @@
 app.ViewerComponent = ViewerComponent;
-app.ViewerComponent.prototype.Users=[];
+app.ViewerComponent.prototype.Users = [];
 ViewerComponent.annotations = [
   new ng.core.Component({
     selector: 'viewer',
     templateUrl: 'app/viewer/viewer.component.html'
   })
 ];
+app.ViewerComponent.prototype.ARROW_DOWN = '▲';
+app.ViewerComponent.prototype.ARROW_UP = '▼';
 
 app.ViewerComponent.prototype.showTable = function () {
   app.ViewerComponent.prototype.isTable = true;
@@ -22,9 +24,54 @@ app.ViewerComponent.prototype.showTiles = function () {
   app.ViewerComponent.prototype.isCards = false;
   app.ViewerComponent.prototype.isTiles = true;
 };
+app.ViewerComponent.prototype.descByName = true;
+app.ViewerComponent.prototype.descByAge = false;
+app.ViewerComponent.prototype.descByGender = false;
+app.ViewerComponent.prototype.descByDepartment = false;
+app.ViewerComponent.prototype.descByAddress = false;
+app.ViewerComponent.prototype.descByPhone = false;
+app.ViewerComponent.prototype.descByEmail = false;
 function ViewerComponent() {
   loadJSON('mock.json',
-         function(data) { app.ViewerComponent.prototype.Users=data; },
-         function(xhr) { alert(xhr); });
+    function (data) {
+      app.ViewerComponent.prototype.Users = data;
+      app.ViewerComponent.prototype.sortByName();
+    },
+    function (xhr) { alert(xhr); });
   app.ViewerComponent.prototype.showTable();
+}
+function dynamicSort(property, isSortDesc) {
+  return function (a, b) {
+    var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+    var sortingDirection = -1 + 2 * isSortDesc;
+    return result * sortingDirection;
+  }
+}
+app.ViewerComponent.prototype.sortByName = function () {
+  app.ViewerComponent.prototype.Users.sort(dynamicSort('name', app.ViewerComponent.prototype.descByName));
+  app.ViewerComponent.prototype.descByName = !app.ViewerComponent.prototype.descByName;
+}
+app.ViewerComponent.prototype.sortByAge = function () {
+  app.ViewerComponent.prototype.Users.sort(dynamicSort('age', this.descByAge));
+  app.ViewerComponent.prototype.descByAge = !app.ViewerComponent.prototype.descByAge;
+}
+app.ViewerComponent.prototype.sortByGender = function () {
+  app.ViewerComponent.prototype.Users.sort(dynamicSort('gender', this.descByGender));
+  app.ViewerComponent.prototype.descByGender = !app.ViewerComponent.prototype.descByGender;
+}
+app.ViewerComponent.prototype.sortByDepartment = function () {
+  app.ViewerComponent.prototype.Users.sort(dynamicSort('department', this.descByDepartment));
+  app.ViewerComponent.prototype.descByDepartment = !app.ViewerComponent.prototype.descByDepartment;
+}
+app.ViewerComponent.prototype.sortByAddress = function () {
+  app.ViewerComponent.prototype.Users.sort(dynamicSort('address', this.descByAddress));
+  app.ViewerComponent.prototype.descByAddress = !app.ViewerComponent.prototype.descByAddress;
+}
+app.ViewerComponent.prototype.sortByPhone = function () {
+  app.ViewerComponent.prototype.Users.sort(dynamicSort('phone', this.descByPhone));
+  app.ViewerComponent.prototype.descByPhone = !app.ViewerComponent.prototype.descByPhone;
+}
+app.ViewerComponent.prototype.sortByEmail = function () {
+  app.ViewerComponent.prototype.Users.sort(dynamicSort('email', this.descByEmail));
+  app.ViewerComponent.prototype.descByEmail = !app.ViewerComponent.prototype.descByEmail;
 }
